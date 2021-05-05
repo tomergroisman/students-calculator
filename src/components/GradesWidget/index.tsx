528cdfad5c3e85b9fedf06f13a08d7629cf6ef4c
Tomer Groisman <tomergroisman@gmail.com>
2021-05-05 17:28:20 +0300

    Add grader widget fade in effect

--- src/components/GradesWidget/index.tsx
+++ src/components/GradesWidget/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Fade from '@material-ui/core/Fade';
 import {GradesHighlights} from '../../utils/types';
 
 interface Props {
@@ -8,28 +9,30 @@ interface Props {
 export default function GradesWidget(props: Props) {
   if (props.gradesHighlights) {
   return (
-    <div>
-      <div className="row">
-        <div className="col-6">
-          <h1 className="text-center display-4">Average</h1>
-          <p className="text-center fs-2">{props.gradesHighlights.avg}</p>
-        </div>
-        <div className="col-6">
-          <h1 className="text-center display-4">Highest</h1>
-          <p className="text-center fs-2">{props.gradesHighlights.highest}</p>
-        </div>
-      </div>
+    <Fade in={!!props.gradesHighlights} timeout={1000}>
+      <div>
         <div className="row">
           <div className="col-6">
-            <h1 className="text-center display-4">Lowest</h1>
-            <p className="text-center fs-2">{props.gradesHighlights.lowest}</p>
+            <h1 className="text-center display-4">Average</h1>
+            <p className="text-center fs-2">{props.gradesHighlights.avg}</p>
           </div>
           <div className="col-6">
-            <h1 className="text-center display-4"># Failures</h1>
-            <p className="text-center fs-2">{props.gradesHighlights.failures}</p>
+            <h1 className="text-center display-4">Highest</h1>
+            <p className="text-center fs-2">{props.gradesHighlights.highest}</p>
           </div>
         </div>
-    </div>
+          <div className="row">
+            <div className="col-6">
+              <h1 className="text-center display-4">Lowest</h1>
+              <p className="text-center fs-2">{props.gradesHighlights.lowest}</p>
+            </div>
+            <div className="col-6">
+              <h1 className="text-center display-4"># Failures</h1>
+              <p className="text-center fs-2">{props.gradesHighlights.failures}</p>
+            </div>
+          </div>
+      </div>
+    </Fade>
   )}
   return null;
 }
